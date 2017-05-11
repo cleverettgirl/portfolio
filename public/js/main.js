07fd2b4430d15910259ba96255a3b6892eb6a28a
Christine <cleverett1@gmail.com>
2017-05-11 16:50:33 -0400

    added scrollto links

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    // console.log("***** ", target)
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+          // .stop() -> stops any current animation on 'html' and 'body'
+            scrollTop: target.offset().top
+            // offset() -> he .offset() method allows us to retrieve the current position of an element (specifically its border box, which excludes margins)
+            // top tells us to bring it all the way to the top. if we had a navbar or didn't want it all the wy at the top, could add `- 80` at the end (or whatever #).
+        }, 1000);
+    }
+  });
+
+
+
+
+
+
+
+})
