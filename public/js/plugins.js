f3a4d340dc9c41133d4148e2a7e94c13c7d9b48a
Christine <cleverett1@gmail.com>
2017-05-30 16:50:22 -0400

    resized pictures for work section

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -1,25 +1,25 @@
 // Avoid `console` errors in browsers that lack a console.
-(function() {
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
+// (function() {
+//     var method;
+//     var noop = function () {};
+//     var methods = [
+//         'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+//         'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+//         'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+//         'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+//     ];
+//     var length = methods.length;
+//     var console = (window.console = window.console || {});
 
-    while (length--) {
-        method = methods[length];
+//     while (length--) {
+//         method = methods[length];
 
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
-        }
-    }
-}());
+//         // Only stub undefined methods.
+//         if (!console[method]) {
+//             console[method] = noop;
+//         }
+//     }
+// }());
 
 // Place any jQuery/helper plugins in here.
 $(function(){
@@ -38,7 +38,7 @@ $(function(){
     const workScene = new ScrollMagic.Scene({
       triggerElement: '#work',
       triggerHook: 'onCenter',
-      duration: $('.projects').height() * .85,
+      duration: $('.projects').height() * .83,
       offset: $('#work').scrollTop()
     })
     .setPin('.work.chapter-title')
@@ -65,14 +65,5 @@ $(function(){
 
     controller.addScene([scale_scene, bg_scene]);
 
-    // const contactScene = new ScrollMagic.Scene({
-    //   triggerElement: "#contact",
-    //   triggerElement: "onCenter",
-    //   duration: $('.text').height() * .83,
-    //   offset: $('#contact').scrollTop()
-    // })
-    // .setPin('.contact.chapter-title')
-    // .addIndicators()
-    // .addTo(controller);
 
 });
