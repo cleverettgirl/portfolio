712b6ccb64183f46743cfc71ddd1c5e6416c6df0
Christine <cleverett1@gmail.com>
2017-06-08 21:02:49 -0400

    scrolling is working for about and  work; not responsive yet

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 $(function(){
 
   // to test distance of about section from top
-  var distance = $('section#about').offset().top;
+  // var distance = $('#slide01').offset().top;
 
   // link scrolling
   $('a[href^="#"]').on('click', function(event) {
@@ -18,6 +18,78 @@ $(function(){
         }, 1000);
     }
   });
+
+
+// *** MAKE WORK & ABOUT SECTION STICKY
+  $(window).on('scroll resize', function () {
+
+    const $about = $('#aboutSlide'),
+          $aboutContent = $('#about-content'),
+          $window = $(window),
+          $aboutHeader = $('#about-header')
+
+    //#about-main
+    let aboutTop = $about.offset().top;
+    let aboutBottom = $about.height() + aboutTop;
+    let aboutHeaderTop = $aboutContent.height() - $window.height();
+
+
+
+    //window
+    let windowTop = $window.scrollTop();
+    let windowBottom = $window.height() + windowTop;
+
+
+    //logic for about
+    if (windowBottom > aboutBottom) {
+      $aboutHeader.removeClass('fixed-header');
+      $aboutHeader.css('top', aboutHeaderTop);
+    }
+    else if (windowTop > aboutTop) {
+      $aboutHeader.addClass('fixed-header');
+      $aboutHeader.css('top', 0);
+    }
+    else {
+      $aboutHeader.removeClass('fixed-header');
+      $aboutHeader.css('top', 0);
+    }
+
+  });
+
+
+
+
+
+// scroll magic:
+
+  var controller = new ScrollMagic.Controller(); // init scrollMagic
+
+    var pinScene02 = new ScrollMagic.Scene({
+      triggerElement: '#slide01',
+      triggerHook: 0, // 0 is the top of the viewport, 0.5 is middle and 1 is bottom of view port
+      duration: '100%' // user needs to scroll 100% of the view port height to unpin this section.
+    })
+    .setPin("#slide01 .pin-wrapper") // pass it the ID of the element we want to pin
+    .addTo(controller)
+
+        // Scene 2 - pin the third section
+    var pinScene03 = new ScrollMagic.Scene({
+      triggerElement: '#slide01',
+      triggerHook: 0, // 0 is the top of the viewport, 0.5 is middle and 1 is bottom of view port --> this new scene doesn't get pinned until it is at the top of the view port!
+      duration: '200%' // user needs to scroll 100% of the view port height to unpin this section.
+    })
+    .setPin("#slide02 .pin-wrapper") // pass it the ID of the element we want to pin
+    .addTo(controller)
+
+    var pinScene04 = new ScrollMagic.Scene({
+      triggerElement: '#slide02',
+      triggerHook: 0,
+      duration: '100%'
+    })
+    .setPin('#slide03 .pin-wrapper')
+    .addTo(controller)
+
+
 
 
 })
--- public/js/plugins.js
+++ public/js/plugins.js
@@ -1,69 +1,29 @@
 // Avoid `console` errors in browsers that lack a console.
-// (function() {
-//     var method;
-//     var noop = function () {};
-//     var methods = [
-//         'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-//         'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-//         'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-//         'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
-//     ];
-//     var length = methods.length;
-//     var console = (window.console = window.console || {});
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
 
-//     while (length--) {
-//         method = methods[length];
+    while (length--) {
+        method = methods[length];
 
-//         // Only stub undefined methods.
-//         if (!console[method]) {
-//             console[method] = noop;
-//         }
-//     }
-// }());
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
 
 // Place any jQuery/helper plugins in here.
 $(function(){
     const controller = new ScrollMagic.Controller();
-
-    const aboutScene = new ScrollMagic.Scene({
-      triggerElement: '#about',
-      triggerHook: 'onCenter',
-      duration: $('.text').height() * .83, // until bottom of '.text' enters screen.
-      offset: $("#about").scrollTop()
-    })
-    .setPin('.about.chapter-title')
-    .addIndicators()
-    .addTo(controller);
-
-    const workScene = new ScrollMagic.Scene({
-      triggerElement: '#work',
-      triggerHook: 'onCenter',
-      duration: $('.projects').height() * .83,
-      offset: $('#work').scrollTop()
-    })
-    .setPin('.work.chapter-title')
-    .addIndicators()
-    .addTo(controller);
-
-    const scale_tween = TweenMax.to("#riddikulus", 1, {
-      ease: Linear.easeNone
-    })
-
-    const bg_tween = TweenMax.to("#snakes", 1, {
-      ease:Linear.easeNone
-    })
-
-    const scale_scene = new ScrollMagic.Scene({
-      triggerElement: "#riddikulus"
-    })
-    .setTween(scale_tween);
-
-    const bg_scene = new ScrollMagic.Scene({
-      triggerElement: "#snakes"
-    })
-    .setTween(bg_tween);
-
-    controller.addScene([scale_scene, bg_scene]);
 
 
 });
